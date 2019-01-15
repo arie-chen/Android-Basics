70283dbbed08b6c2043f6384ae1251922b11ca51
Arie Chen <chens.arie@gmail.com>
2019-01-14 19:26:50 -0800

    Fixed color backgrounds for categories

--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.app.Activity;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -12,8 +13,11 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int color;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int color) {
         super(context, 0, words);
+        this.color = color;
     }
 
     @Override
@@ -32,6 +36,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView miwokWord = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokWord.setText(currentWord.getMiwokTranslation());
         ImageView image = (ImageView) listItemView.findViewById(R.id.image);
+        LinearLayout linear = (LinearLayout) listItemView.findViewById(R.id.category);
+        linear.setBackgroundResource(color);
 
         if (currentWord.getImageResourceID() == Word.NO_PICTURE) {
             image.setVisibility(View.GONE);
